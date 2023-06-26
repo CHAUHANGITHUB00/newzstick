9781aca22c6ea9006d27661a7153a47508548ba1
CHAUHANGITHUB00 <137784715+CHAUHANGITHUB00@users.noreply.github.com>
2023-06-26 21:46:53 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.newzstick.com/`
+					`https://tomholland04.blogspot.com/`
 				}`,
 			},
 		};
